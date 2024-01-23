6076c902ece428f7834a3a76ce093623c2efea48
Sam Thomas <samuel.mmthomas@gmail.com>
2024-01-23 09:12:25 +0000

    Initial Commit

---
+++ Download Manager/Download_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Download_ManagerApp.swift
+//  Download Manager
+//
+//  Created by Sam Thomas on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Download_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
